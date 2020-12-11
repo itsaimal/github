37ef814e343857554fa0bbe9c599e36e0d71667b
itsaimal <itsaimalyousafzai@gmail.com>
2020-12-11 14:15:09 +0500

    apis for prac

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Row from "./Row";
 
 function App() {
   return (
     <div className="App">
      <h1>hello app</h1>
+     <Row title="NETFLIX ORIGNALS"/>
+     <Row title="Trending Now"/>
     </div>
   );
 }
