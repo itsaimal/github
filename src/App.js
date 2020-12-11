812e66da6767a7e9e62d3504e8bcee5d7be8f094
itsaimal <itsaimalyousafzai@gmail.com>
2020-12-11 14:38:14 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Row from "./Row"
+import request from "./requests"
 
 function App() {
   return (
     <div className="App">
      <h1>hello app</h1>
+     <Row title="NETFLIX ORIGNALS" fetchUrl={request.}/>
+     <Row title="Trending Now" fetchUrl={request.}/>
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import "./Row.css"
+
+function Row({title}) {
+    const [movies, setMovies] = useState([]);
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+        </div>
+    )
+}
+
+export default Row
